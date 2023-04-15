6e171beddcfba0bf90bd2d65d1ec85eed3867a0b
jnomikos <john.nomikos55@gmail.com>
2023-04-14 22:50:17 -0500

    Server.js is root of program

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Sets our view engine for express which is ejs.
+
+app.set('view engine', 'ejs');
+
+// Whenever a user sends a get request (loads up webpage) at index, we will render index.html
+app.get("/", (req, res) => {
+    console.log("Here");
+    res.render("index", {text: "Test"});
+});
+
+// The about page
+const aboutRouter = require('./routes/about');
+app.use('/about', aboutRouter);
+
+// Choose what port the server will be on. If running this on your local machine, type localhost:3000 in your browser
+app.listen(3000);
