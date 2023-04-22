9edc5b4b4f7a74bb8c7cb33919bf19ebf21bc6b8
jnomikos <john.nomikos55@gmail.com>
2023-04-22 14:20:20 -0500

    Login now works and stores logged in session as a cookie

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+
+// Open database
+let db = new sqlite3.Database('database/mydatabase.db');
 
 router.get("/", (req, res) => {
     console.log("Login");
     res.render("login");
 });
+
+router.post("/", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    checkValidEmployee(username, password).then(isValid => {
+        if(isValid) {
+            let session=req.session;
+            session.username=req.body.username;
+            res.redirect('/');
+        } else {
+            res.render("login", {warning: "Invalid username or password!", username: username, password: password});
+        }
+    })
+   
+})
+
+// Checks if employee exists in our database with matching username and password
+// Note: This is not a secure method of doing this.
+function checkValidEmployee(username, password) {
+    // We return a promise here since db.get is async
+    return new Promise((resolve, reject) => {
+        db.get(`SELECT * FROM employee WHERE name = "${username}" AND password = "${password}"`, (err, row)=>{
+            if (err) {
+                return reject(err);
+            }
+
+            if(row) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+
+}
 
 module.exports = router;
