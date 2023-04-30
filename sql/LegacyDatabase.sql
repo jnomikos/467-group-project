d63b0049b70311087ba865971e70c95745b6735a
jnomikos <john.nomikos55@gmail.com>
2023-04-30 13:54:05 -0500

    Added matt's purchase table

--- sql/LegacyDatabase.sql
+++ sql/LegacyDatabase.sql
@@ -6,7 +6,7 @@ CREATE TABLE employee (
   name VARCHAR(50) NOT NULL,
   address VARCHAR(50),
   city VARCHAR(50),
-  state VARCHAR(50),
+  state VARCHAR(2),
   contact VARCHAR(50),
   commission INTEGER DEFAULT(0),
   isAdmin INTEGER DEFAULT(0)
@@ -22,4 +22,16 @@ CREATE TABLE quote (
   description VARCHAR(50) NOT NULL,
   status VARCHAR(50) DEFAULT('unresolved') NOT NULL,
   FOREIGN KEY (employeeID) REFERENCES employee(employeeID)
+);
+
+CREATE Table purchased (
+  purchasedID INTEGER PRIMARY KEY AUTOINCREMENT,
+  customerID INTEGER NOT NULL,
+  quoteID INTEGER NOT NULL,
+  customerEmail VARCHAR(50) NOT NULL,
+  paymentInfo VARCHAR(50) NOT NULL,
+  price INTEGER NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  FOREIGN KEY (customerID) REFERENCES quote(customerID),
+  FOREIGN KEY (quoteID) REFERENCES quote(quoteID)
 );
