de715c17286633c1d9835fcf8e5d21599c6df53a
jnomikos <john.nomikos55@gmail.com>
2023-04-30 14:02:09 -0500

    Removed purchased table

--- sql/LegacyDatabase.sql
+++ sql/LegacyDatabase.sql
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE admin (
-  adminID INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL,
-  address VARCHAR(50) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  state VARCHAR(50) NOT NULL,
-  contact VARCHAR(50) NOT NULL
-);
-=======
->>>>>>> d819c5ea692b7d2aa85f8ed165cc9491f227fb2c
 
 CREATE TABLE employee (
   employeeID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,16 +22,4 @@ CREATE TABLE quote (
   description VARCHAR(50) NOT NULL,
   status VARCHAR(50) DEFAULT('unresolved') NOT NULL,
   FOREIGN KEY (employeeID) REFERENCES employee(employeeID)
-);
-
-CREATE Table purchased (
-  purchasedID INTEGER PRIMARY KEY AUTOINCREMENT,
-  customerID INTEGER NOT NULL,
-  quoteID INTEGER NOT NULL,
-  customerEmail VARCHAR(50) NOT NULL,
-  paymentInfo VARCHAR(50) NOT NULL,
-  price INTEGER NOT NULL,
-  description VARCHAR(50) NOT NULL,
-  FOREIGN KEY (customerID) REFERENCES quote(customerID),
-  FOREIGN KEY (quoteID) REFERENCES quote(quoteID)
 );
