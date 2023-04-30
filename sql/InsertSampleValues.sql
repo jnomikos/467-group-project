9a2e2413ec5e11f08062f1c2a910f13749901fa5
jnomikos <john.nomikos55@gmail.com>
2023-04-30 00:02:10 -0500

    Added cooler first and last names

--- sql/InsertSampleValues.sql
+++ sql/InsertSampleValues.sql
@@ -1,12 +1,12 @@
 INSERT INTO employee (name, username, password, address, city, state, contact, commission, isAdmin) VALUES 
-('Bob', 'Bob', 'password', '1234 West St', 'Dekalb', 'Illinois', '123-456-7890', 35, 1),
-('Sally', 'Sally', 'password', '1234 East St', 'Dekalb', 'Illinois', '256-726-7361', 40, 1);
+('Bob Williams', 'Bob', 'password', '1234 West St', 'Dekalb', 'Illinois', '123-456-7890', 35, 1),
+('Sally Smith', 'Sally', 'password', '1234 East St', 'Dekalb', 'Illinois', '256-726-7361', 40, 1);
 
 
 INSERT INTO employee(name, username, password, address, city, state, contact, commission) VALUES
-('Bobbie', 'Bobbie', 'password', '800 North street', 'Chicago', 'Illinois', '779-456-1264', 10),
-('Sam', 'SamIAm123', 'password', '1234 East St', 'Dekalb', 'Illinois', '425-954-8245', 10),
-('John', 'JohnIsAwesome', 'password', '1234 North St', 'Dekalb', 'Illinois', '156-456-3645', 10);
+('Bobbie Hill', 'Bobbie', 'password', '800 North street', 'Chicago', 'Illinois', '779-456-1264', 10),
+('Sam Johnson', 'SamIAm123', 'password', '1234 East St', 'Dekalb', 'Illinois', '425-954-8245', 10),
+('John Doe', 'JohnIsAwesome', 'password', '1234 North St', 'Dekalb', 'Illinois', '156-456-3645', 10);
 
 
 INSERT INTO quote(quoteID, customerID, employeeID, customerEmail, paymentInfo, price, description) VALUES 
