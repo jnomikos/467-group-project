58822fc895db7d6ff5a7b5dcd88e0ee539dfee99
mkeisel <z1865716@students.niu.edu>
2023-04-30 13:30:00 -0500

    prototype convertedQuotes table

--- sql/LegacyDatabase.sql
+++ sql/LegacyDatabase.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE admin (
   adminID INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(50) NOT NULL,
@@ -31,4 +29,16 @@ CREATE TABLE quote (
   description VARCHAR(50) NOT NULL,
   status VARCHAR(50) DEFAULT('unresolved') NOT NULL,
   FOREIGN KEY (employeeID) REFERENCES employee(employeeID)
+);
+
+CREATE Table purchased (
+  purchasedID INTEGER PRIMARY KEY AUTOINCREMENT,
+  customerID INTEGER NOT NULL,
+  quoteID INTEGER NOT NULL,
+  customerEmail VARCHAR(50) NOT NULL,
+  paymentInfo VARCHAR(50) NOT NULL,
+  price INTEGER NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  FOREIGN KEY (customerID) REFERENCES quote(customerID),
+  FOREIGN KEY (quoteID) REFERENCES quote(quoteID)
 );
